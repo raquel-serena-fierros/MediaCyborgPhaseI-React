ab4808a9b8373650be6d44b32d863bf5c6687056
Raquel Serena Fierros <86587161+raquel-serena-fierros@users.noreply.github.com>
2023-02-16 17:21:54 -0500

    Content changes
    Text Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
           The Next <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span>{" "}
+          <span className="text-gradient">Optimized Stardom</span>{" "}
         </h1>
       </div>
 
